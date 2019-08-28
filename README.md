12439c939c93bd3fb65a67c89452a52d9da5f54d
Glitch (sponge-pearl) <none>
2019-08-28 09:33:03 +0000

    ⏰🍄 Updated with Glitch

--- README.md
+++
@@ -1,1 +0,0 @@
-# TurkishMP-Bot
--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('**' + username + '** Welcome To TurkishMP Official Discord Server! Role the language you want from #languages :heart:');
-
-};
